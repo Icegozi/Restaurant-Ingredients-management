fe70c8e5871803ea956d5dad31e0096f5920709c
icegozi <haxuanphuc48@gmail.com>
2024-11-09 00:16:40 +0700

    create layout

--- app/src/main/java/com/example/restaurant_ingredients_management/MainActivity.java
+++ app/src/main/java/com/example/restaurant_ingredients_management/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.restaurant_ingredients_management;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.restaurant_ingredients_management.View.GiaoDich;
+import com.example.restaurant_ingredients_management.View.TongQuan;
+
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+         btn = findViewById(R.id.test);
+         btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent next = new Intent(MainActivity.this, TongQuan.class);
+                    startActivity(next);
+             }
+         });
     }
 }
